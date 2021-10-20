067f62ecae37e5f9f5b4c5fd3d9137d8c4fbfa43
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-10-20 00:37:19 -0500

    Get, POST method in aws lambda, Api gateway, DynamoDB added

--- requestPizzas/handler.js
+++ requestPizzas/handler.js
@@ -9,7 +9,6 @@ const orderMetadataManager = require('./orderMetadataManager');
 
 module.exports.hacerPedido = async (event) => {
   console.log("Hacer pedido fue llamada");
-  console.log("Event", event);
   const orderId = uuidv4();
 
   const body = JSON.parse(event.body);
@@ -53,16 +52,6 @@ module.exports.hacerPedido = async (event) => {
 
 module.exports.prepararPedido = async (event) => {
   console.log("prepararPedido fue llamada");
-
-  // const order = JSON.parse(event.Records[0].body);
-  // orderMetadataManager.saveCompleteOrder(order)
-  //                     .then(data =>{
-  //                       console.log("data", data);
-  //                       // return data;
-  //                     })
-  //                     .catch(err =>{
-  //                       console.log("err", err);
-  //                     });
   const order = JSON.parse(event.Records[0].body);
 
 	const sqsResponse = await orderMetadataManager
@@ -70,9 +59,62 @@ module.exports.prepararPedido = async (event) => {
 		.then(data => {
 			console.log("data", data);
 		})
-		.catch(error => {
+		.catch(err => {
 			console.log("err", err);
 		});
+};
+
+module.exports.enviarPedido = async (event) => {
+  console.log('Llama a enviarPedido');
+  const record = event.Records[0];
+	if (record.eventName === 'INSERT') {
+		console.log('deliverOrder');
+
+		const orderId = record.dynamodb.Keys.orderId.S;
+
+		const sqsResponse = await orderMetadataManager
+			.deliverOrder(orderId)
+			.then(data => {
+				console.log("data", data);
+			})
+			.catch(err => {
+				console.log("err", err);
+			});
+	} else {
+		console.log('is not a new record');
+	}
+};
+
+module.exports.estadoPedido = async (event) => {
+	console.log('Estado pedido fue llamado');
+  console.log('Estado pedido fue llamado event', event);
+	const orderId = event.pathParameters && event.pathParameters.orderId;
+  console.log('Estado pedido fue llamado orderId', orderId);
+	if (orderId !== null) {
+		const respOrder = await orderMetadataManager
+			.getOrder(orderId)
+			.then(order => {      
+        const dataResponse = {
+          orderId: order.orderId,
+          status: order.delivery_status,
+          name: order.name
+        }
+        return dataResponse;
+				
+			})
+			.catch(error => {
+				console.log(500, 'Hubo un error al procesar el pedido',error);
+			});
+
+      if(respOrder !=null){
+        console.log(200, `El estado de la orden ${respOrder}`);
+        return responseWs(200, `El estado de la orden: ${respOrder.name} y  ${respOrder.orderId} es ${respOrder.status}`);
+      }else{
+        return responseWs(500, 'Hubo un error al procesar el pedido');
+      }
+	} else {
+		return responseWs(400, 'Falta el orderId');
+	}
 };
 
 const responseWs = (statusCode, message) =>{
